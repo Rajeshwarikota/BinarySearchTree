374e6039de9ae3faff82191208c04810f1c9b19a
Rajeshwarikota <kotarajeswari003.com>
2023-02-24 12:48:50 +0530

    [Rajeshwari] Adding Binary tree search method

--- BinarySearchTree/BinaryTree.cs
+++ BinarySearchTree/BinaryTree.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 
@@ -11,13 +12,14 @@ namespace BinarySearchTree
     public class BinaryTree<T> where T : IComparable
     {
         public Node<T> root { get; set; } = null;
+        public int count = 0;
         public  void insert(T data)
         {
-           
-
             if (root == null)
             {
                 root = new Node<T>(data);
+                count++;
+                
             }
             Node<T> current = root;
             while (current != null)
@@ -27,9 +29,11 @@ namespace BinarySearchTree
                     if (current.right != null)
                     {
                         current = current.right;
-                        break;
+                       
                     }
                     current.right = new Node<T>(data);
+                    count++;
+                    break;
                 }
 
                 else if (data.CompareTo(current.nodedata) < 0)
@@ -37,9 +41,11 @@ namespace BinarySearchTree
                     if (current.left != null)
                     {
                         current = current.left;
-                        break;
+                        
                     }
                     current.left = new Node<T>(data);
+                    count++;
+                    break;
                 }
 
                 else
@@ -50,6 +56,40 @@ namespace BinarySearchTree
             }
            
         }
+        public int Size()
+        {
+            return count;
+        }
+        public Node<T> Search(T value, Node<T> root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            else if (value.CompareTo(root.nodedata) < 0)
+            {
+                return Search(value, root.left);
+            }
+            else if (value.CompareTo(root.nodedata) > 0)
+            {
+                return Search(value, root.right);
+            }
+            else
+            {
+                return root;
+            }
+        }
+        public void isExists(T data, Node<T> root)
+        {
+            Node<T> node = Search(data, root);
+            if (node != null)
+            {
+
+                Console.WriteLine("Element Found :" + node.nodedata);
+            }
+            else
+                Console.WriteLine("Element {0} is not Found ", data);
+        }
         public void Display(Node<T> root)
         {
             if (root == null)
@@ -59,6 +99,16 @@ namespace BinarySearchTree
             Display(root.left);
             System.Console.Write(root.nodedata + " ");
             Display(root.right);
+        }
+        public int size(Node<T> root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            int leftSize = size(root.left);
+            int rightSize = size(root.right);
+            return leftSize + rightSize + 1;
         }
     }
 }
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -11,14 +11,47 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("....Binary Search Tree....");
-            BinaryTree<int> binarySearch = new BinaryTree<int>();
-            binarySearch.insert(56);
-            binarySearch.insert(30);
-            binarySearch.insert(70);
-            binarySearch.Display(binarySearch.root);
-            Console.ReadLine();
+            
 
-       
+            Console.WriteLine("\n1.Insertion \n2:Insert More Elements \n3:searching element");
+           
+
+            Console.WriteLine("Enter the option!!!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            BinaryTree<int> binarySearch = new BinaryTree<int>();
+            switch (num)
+            {
+                case 1:
+                    binarySearch.insert(56);
+                    binarySearch.insert(30);
+                    binarySearch.insert(70);
+                    binarySearch.Display(binarySearch.root);
+                    break;
+                case 2:
+                    binarySearch.insert(56);
+                    binarySearch.insert(30);
+                    binarySearch.insert(70);
+                    binarySearch.insert(22);
+                    binarySearch.insert(60);
+                    binarySearch.insert(40);
+                    binarySearch.insert(95);
+                    binarySearch.insert(65);
+                    binarySearch.insert(11);
+                    binarySearch.insert(3);
+                    binarySearch.insert(16);
+                    binarySearch.insert(63);
+                    binarySearch.insert(67);
+                    binarySearch.Display(binarySearch.root);
+                    Console.WriteLine("\nTotal Number of elements =" + binarySearch.Size());
+                    break;
+                case 3:
+                    binarySearch.isExists(63, binarySearch.root);
+                    break;
+                default:
+                    Console.WriteLine("Enter above option");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
